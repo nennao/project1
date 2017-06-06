c250f62b52f6328d5417403157175a388ff99603
Nenna Okoye <ninaokoye@outlook.com>
2017-06-07 00:42:08 +0100

    improved home > events' hexagons

--- js/controller.js
+++ js/controller.js
@@ -5,16 +5,13 @@ angular.module("RouteControllers", [])
 		$('#home-carousel').carousel('cycle');
 
 		// toggle events display
-		$('.hex1 hex, .hex1 .frontTxt, .hex1 .backTxt').on('click', function() {
-			$('.hex1 .hexImg').toggleClass('hexFade');
+		$('.hex1 .hexagon, .hex1 .hexClick, .hex1 .backTxt').click(function() {
 	    	$('.hex1 .frontTxt, .hex1 .backTxt').fadeToggle();
 		});
-		$('.hex2 hex, .hex2 .frontTxt, .hex2 .backTxt').on('click', function() {
-			$('.hex2 .hexImg').toggleClass('hexFade');
+		$('.hex2 .hexagon, .hex2 .hexClick, .hex2 .backTxt').click(function() {
 	    	$('.hex2 .frontTxt, .hex2 .backTxt').fadeToggle();
 		});
-		$('.hex3 hex, .hex3 .frontTxt, .hex3 .backTxt').on('click', function() {
-			$('.hex3 .hexImg').toggleClass('hexFade');
+		$('.hex3 .hexagon, .hex3 .hexClick, .hex3 .backTxt').click(function() {
 	    	$('.hex3 .frontTxt, .hex3 .backTxt').fadeToggle();
 		});
 		
